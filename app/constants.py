005318cd232561573be03ff661a5851e90d2167e
anarkrypto <anarkrypto@gmail.com>
2025-07-07 19:32:15 -0300

    feat(constants): add USDC contract addresses for mainnet and sepolia

--- app/constants.py
+++ app/constants.py
@@ -26,3 +26,9 @@ NETWORKS = {
         symbol=ChainId(11155111),
     ),
 }
+
+# Source: https://developers.circle.com/stablecoins/usdc-contract-addresses
+USDC_CONTRACTS: Dict[NetworkType, str] = {
+    'mainnet': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    'sepolia': '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
+}
