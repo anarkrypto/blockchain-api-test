2ee025a14892b0fdd3a1bae16813327cf71a54e6
anarkrypto <anarkrypto@gmail.com>
2025-07-06 22:13:45 -0300

    feat: create addresses table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,18 @@
+from sqlalchemy import Column, DateTime, Integer, String, text
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+
+    address = Column(String, primary_key=True)
+    index = Column(Integer, unique=True, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=text('CURRENT_TIMESTAMP'),
+    )
