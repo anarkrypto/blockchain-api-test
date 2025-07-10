065cb28d3a352566275ed56f7b94cd2dccde76e8
anarkrypto <anarkrypto@gmail.com>
2025-07-10 01:12:19 -0300

    feat: configure basic logger

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,3 +1,13 @@
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
